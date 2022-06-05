9f59ea11cbe0772ae730d64c9b9744083dca3594
Domnic Omondi <domooh9@gmail.com>
2022-06-05 23:08:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+
+
+    const input = document.getElementById('input');
+    input.addEventListener('click', clickAlert);
+    function clickAlert() {
+        alert('I was clicked!');
+    }
 }
+
+addingEventListener; 
